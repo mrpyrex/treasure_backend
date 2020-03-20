488d0efa8f03e91d5ba0ec0697fa393c1802df18
mrpyrex <ndifrkeumoren@gmail.com>
2020-03-20 07:54:37 +0100

    Added Heroku Django

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
